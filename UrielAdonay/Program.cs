54e33aa00ab265de721fa3be518826aeb260c101
Monzón <58608455+Diaser@users.noreply.github.com>
2023-06-10 11:35:51 -0600

    first commit

---
+++ UrielAdonay/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UrielAdonay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido a mi Programa");
+        }
+    }
+}
